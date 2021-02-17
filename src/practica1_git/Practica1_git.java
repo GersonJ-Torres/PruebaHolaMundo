f3ef6ed3c924886ccf9f49230ab9aa89179928e5
GersonJ-Torres <torresmoralesgj@gmail.com>
2021-02-16 20:40:22 -0600

    Esta es mi primera version

---
+++ src/practica1_git/Practica1_git.java
@@ -0,0 +1,13 @@
+package practica1_git;
+
+/**
+ * @author Torres Morales Gerson Jireh
+ */
+public class Practica1_git {
+
+    public static void main(String[] args) {
+        // Escribir en consola: "Hello World!"
+        System.out.println("Hello World!");
+    }
+    
+}
